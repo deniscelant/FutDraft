6ffb8844d2ccd50cf238cd71b15f9317a43be8b8
deniscelant <101429050+deniscelant@users.noreply.github.com>
2025-04-14 22:50:43 -0300

    logic

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-import * as db from "./db"
+import * as db from "./db";
 
 let draft = false;
 
 window.addEventListener("DOMContentLoaded", () => {
-    draft = false;
-})
-
+  draft = false;
+});
+ 
 /* regra de negócio
 
     sortear informações para a carta
@@ -13,64 +13,81 @@ window.addEventListener("DOMContentLoaded", () => {
     clube = clubs[mt].club
     name = names[mt]
     nations = nations[mt]
+    
+    // physics and stats logic
+    positions = ZG, LD, VOL, MC, MD, ME, MEI, SA, AT, PD, PE
+    
+    any = Math.floor(Math.random() * 99)
+    under60 = Math.floor(Math.random() * (60 - 10))
+    over60 = Math.floor(Math.random() * (99 - 60))
+
+    
+    var phys = [
+        {   
+            pace: physics.pace,
+            resistance: 99,
+            impulse: 99,
+            strength: 99,
+            height: 1.99,
+            weight: 80,
+        }
+    ]
 
 */
 
-class Draft{
-    constructor(){
-        renderFormations();
-        renderLineUp();
-    }
+console.log(Math.floor(Math.random() * 99))
+   
+class Draft {
+  constructor() {
+    renderFormations();
+    renderLineUp();
+  }
 
-    renderFormations(){
-        const formationBoard = document.createElement("div");
-        db.formations.forEach(f => {
-            const formt = document.createElement("div")
-            formt.textContent = f.lines
-        })
-    }
+  renderFormations() {
+    const formationBoard = document.createElement("div");
+    db.formations.forEach((f) => {
+      const formt = document.createElement("div");
+      formt.textContent = f.lines;
+    });
+  }
 }
 
-class Player{
-    constructor(name, nation, club, league, physics, stats){
-        name = this.name;
-        nation = this.nation;
-        club = this.club;
-        league = this.league;
-        physics = this.physics;
-        stats = this.stats;
-    }
+class Player {
+  constructor(name, nation, club, league, physics, stats) {
+    name = this.name;
+    nation = this.nation;
+    club = this.club;
+    league = this.league;
+    physics = this.physics;
+    stats = this.stats;
+  }
 }
 
-
-class PlayerPick{
-    constructor(players, cap){
-        players = this.players;
-        cap = this.cap;
-    }
+class PlayerPick {
+  constructor(players, cap) {
+    players = this.players;
+    cap = this.cap;
+  }
 }
 
-function Main(){
-    if(!draft){
-        new Draft();
-    } else{
-        throw console.error("Não há drafts iniciados.");
-        
-    }
+function Main() {
+  if (!draft) {
+    new Draft();
+  } else {
+    throw console.error("Não há drafts iniciados.");
+  }
 }
 
-class RenderCards{
-    constructor(){
-        const card = document.createElement("div");
-        const picture = document.createElement("img");
-        const info = document.createElement("div");
-        const list = document.createElement("li");
+class RenderCards {
+  constructor() {
+    const card = document.createElement("div");
+    const picture = document.createElement("img");
+    const info = document.createElement("div");
+    const list = document.createElement("li");
 
-        card.classList.add("card")
-        card.appendChild(picture)
-        card.appendChild(info)
-        info.appendChild(list)
-
-    }
-
+    card.classList.add("card");
+    card.appendChild(picture);
+    card.appendChild(info);
+    info.appendChild(list);
+  }
 }
