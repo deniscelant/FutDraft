73f39ff7564768279479eab96705ad83b1d98b3d
deniscelant <101429050+deniscelant@users.noreply.github.com>
2025-04-16 20:08:49 -0300

    playerCore

--- app.js
+++ app.js
@@ -2,42 +2,38 @@ import * as db from "/db.js";
 
 let cardStats = {};
 
-const button = document.getElementById("btn")
+const button = document.getElementById("btn");
 button.onclick = () => {
+  const buttonText = button.textContent;
 
-  const buttonText = button.textContent
-
-  switch(buttonText){
+  switch (buttonText) {
     case "ZG":
       cardStats = Object.create(db.ZG);
       break;
     case "LDLE":
       cardStats = Object.create(db.LDLE);
       break;
-      case "VOL":
+    case "VOL":
       cardStats = Object.create(db.VOL);
       break;
-      case "MC":
+    case "MC":
       cardStats = Object.create(db.MC);
       break;
-      case "MEMD":
+    case "MEMD":
       cardStats = Object.create(db.MEMD);
       break;
-      case "SAMEI":
+    case "SAMEI":
       cardStats = Object.create(db.SAMEI);
       break;
-      case "PEPD":
+    case "PEPD":
       cardStats = Object.create(db.PEPD);
       break;
-      case "CA":
+    case "CA":
       cardStats = Object.create(db.CA);
-    
-
   }
-  const player = new Player()
-  player.renderCard()
-
-}
+  const player = new Player();
+  player.renderCard();
+};
 const any = (array) => {
   Math.floor(Math.random() * array.length);
 };
@@ -57,61 +53,68 @@ class Draft {
   renderFormations() {
     const formationBoard = document.createElement("div");
     const formt = document.createElement("div");
-    document.body.appendChild(formationBoard)
-    formationBoard.appendChild(formt)
-    
+    document.body.appendChild(formationBoard);
+    formationBoard.appendChild(formt);
+
     formt.textContent = f.lines;
 
     //formation for f.lines
   }
 }
- 
+
 class Player {
   constructor() {
-    this.nome = db.names[any(db.names)];
-    this.nation = db.nations[any(db.nations)];
+    this.nome = db.names[Math.floor(Math.random() * db.names.length)];
+    this.nation = db.nations[Math.floor(Math.random() * db.nations.length)];
     this.club = db.clubs[Math.floor(Math.random() * db.clubs.length)].club;
     this.posDB = cardStats;
     this.card = document.createElement("div");
     this.card.id = "card";
 
-    this.info = document.createElement("div")
-    this.info.id = "info"
-    
-    document.body.appendChild(this.card)
-    this.card.appendChild(this.info)
+    this.cardContent = document.createElement("div");
+    this.cardContent.id = "cardContent";
+
+    document.body.appendChild(this.card);
+    this.card.appendChild(this.cardContent);
   }
 
   renderCard() {
-    this.info.innerHTML = `
-      <div id="cardStats">
-          <ul>
-            <li>Pass</li>
-            <li id="pass_stat">${cardStats.pass}</li>
-            <li>Dribble</li>
-            <li id="dribble_stat">${cardStats.dribble}</li>
-            <li>Desarm</li>
-            <li id="desarm_stat">${cardStats.desarm}</li>
-            <li>Shoot</li>
-            <li id="shoot_stat">${cardStats.shoot}</li>
-          </ul>
-        </div>
+    this.cardContent.innerHTML = `
+    <div id ="playerCore">
+        <p class="playerInfo">${this.nome}</p>
+        <p class="playerInfo">${this.nation}</p>
+        <p class="playerInfo">${this.club}</p>
+    </div>
+      <div id="info">
+        <div id="infoStats">
+            <ul>
+              <li>Pass</li>
+              <li id="pass_stat">${cardStats.pass}</li>
+              <li>Dribble</li>
+              <li id="dribble_stat">${cardStats.dribble}</li>
+              <li>Desarm</li>
+              <li id="desarm_stat">${cardStats.desarm}</li>
+              <li>Shoot</li>
+              <li id="shoot_stat">${cardStats.shoot}</li>
+            </ul>
+          </div>
 
-        <div id="cardPhysics">
-          <ul>
-            <li>Pace</li>
-            <li id="pace_stat">${cardStats.pace}</li>
-            <li>Resistance</li>
-            <li id="resistance_stat">${cardStats.resistance}</li>
-            <li>Impulse</li>
-            <li id="impulse_stat">${cardStats.impulse}</li>
-            <li>Strength</li>
-            <li id="strength_stat">${cardStats.strength}</li>
-          </ul>
-        </div>
+          <div id="infoPhysics">
+            <ul>
+              <li>Pace</li>
+              <li id="pace_stat">${cardStats.pace}</li>
+              <li>Resistance</li>
+              <li id="resistance_stat">${cardStats.resistance}</li>
+              <li>Impulse</li>
+              <li id="impulse_stat">${cardStats.impulse}</li>
+              <li>Strength</li>
+              <li id="strength_stat">${cardStats.strength}</li>
+            </ul>
+          </div>
+          
       </div>
-    `
 
+    `;
   }
 }
 
@@ -129,4 +132,3 @@ function init() {
     throw console.error("Não há drafts iniciados.");
   }
 }
-
